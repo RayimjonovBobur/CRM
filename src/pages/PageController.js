8badcbd62391156c8af288aa3e3ee7e306a2b680
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-04-01 15:59:06 +0500

    Update PageController.js

--- src/pages/PageController.js
+++ src/pages/PageController.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {
-    SERVIS_PAGE,
-    MAIN_PAGE,
-    PROGRAMMES_PAGE,
-    REPORT_PAGE,
-    SERVIS_CHILD_PAGES,
-    CLIENTS_CHILD_PAGES,
-    CLIENTS_PAGE,
-    PROGRAMMERS_CHILD_PAGES,
+  SERVIS_PAGE,
+  MAIN_PAGE,
+  PROGRAMMES_PAGE,
+  REPORT_PAGE,
+  SERVIS_CHILD_PAGES,
+  CLIENTS_CHILD_PAGES,
+  CLIENTS_PAGE,
+  PROGRAMMERS_CHILD_PAGES,
 } from "./pageConstants/PageTypes";
 import FirstPage from "./mainPage/FirstPage";
 import ProgrammesPage from "./programmesPage/ProgrammesPage";
@@ -16,26 +16,26 @@ import ReportPage from "./reportPage/ReportPage";
 import ServicePageChild from "./servicePage/ServicePageChild/ServicePageChild";
 import ClientsPage from "./clientsPage/ClientsPage";
 
-
 export const PageController = ({ page }) => {
-    switch (page.type) {
-        case MAIN_PAGE:
-            return <FirstPage page={page} />;
-        case CLIENTS_PAGE:
-            return <ClientsPage page={page} />;
-        case PROGRAMMES_PAGE:
-            return <ProgrammesPage page={page} />;
-        case SERVIS_PAGE:
-            return <ServicePage page={page} />;
-        case REPORT_PAGE:
-            return <ReportPage page={page} />;
-        case SERVIS_CHILD_PAGES:
-            return <ServicePageChild page={page} />;
-        case CLIENTS_CHILD_PAGES:
-            return <ClientsPage page={page} />;
-        case PROGRAMMERS_CHILD_PAGES:
-            return <ProgrammesPage page={page} />;
-        default:
-            return <h1>Not found</h1>;
-    }
+  switch (page.type) {
+    case MAIN_PAGE:
+      return <FirstPage page={page} />;
+    case CLIENTS_PAGE:
+      return <ClientsPage page={page} />;
+    case PROGRAMMES_PAGE:
+      return <ProgrammesPage page={page} />;
+    case SERVIS_PAGE:
+      return <ServicePage page={page} />;
+    case REPORT_PAGE:
+      return <ReportPage page={page} />;
+
+    case SERVIS_CHILD_PAGES:
+      return <ServicePageChild page={page} />;
+    case CLIENTS_CHILD_PAGES:
+      return <ClientsPage page={page} />;
+    case PROGRAMMERS_CHILD_PAGES:
+      return <ProgrammesPage page={page} />;
+    default:
+      return <h1>Not found</h1>;
+  }
 };
